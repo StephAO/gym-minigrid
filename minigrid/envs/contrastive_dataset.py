4f8d434874dc854b9d3fd08b2544a7101c02b505
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2023-01-05 14:35:53 -0700

    add splits

--- minigrid/envs/contrastive_dataset.py
+++ minigrid/envs/contrastive_dataset.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+import itertools
+import os
+import random
+
 from minigrid.core.constants import COLOR_NAMES
 from minigrid.core.grid import Grid
 from minigrid.core.mission import MissionSpace
-from minigrid.core.constants import COLOR_NAMES, OBJECT_NAMES, OBJECT_TO_IDX, COLOR_TO_IDX
+from minigrid.core.constants import COLOR_NAMES, NON_BASE_OBJ_NAMES, OBJECT_TO_IDX, COLOR_TO_IDX
 from minigrid.core.world_object import WorldObj
 from minigrid.minigrid_env import MiniGridEnv
 
@@ -13,18 +17,26 @@ class ContrastiveDataset(MiniGridEnv):
     Environment in which the agent is instructed to go to a given object
     named using an English text string
     """
+    def __init__(self, size=7, numObjs=1, splits=(0.7, 0.1, 0.2), max_steps: int | None = None, **kwargs):
 
-    def __init__(self, size=7, numObjs=1, max_steps: int | None = None, **kwargs):
-
-        self.numObjs = numObjs
+        # self.numObjs = numObjs
         self.size = size
         # Types of objects to be generated
 
         self.obj_types = NON_BASE_OBJ_NAMES
+        self.col_types = COLOR_NAMES
+        self.all_compositions = list(itertools.product(self.col_types, self.obj_types))
+        random.shuffle(self.all_compositions)
+        splits = int(splits[0] * len(self.all_compositions)), int(sum(splits[:2]) * len(self.all_compositions))
+        self.splits = {'train': self.all_compositions[:splits[0]],
+                       'val': self.all_compositions[splits[0]:splits[1]],
+                       'test': self.all_compositions[splits[1]:]}
+        self.curr_split = 'train'
+
 
         mission_space = MissionSpace(
             mission_func=self._gen_mission,
-            ordered_placeholders=[COLOR_NAMES, self.obj_types],
+            ordered_placeholders=[self.col_types, self.obj_types],
         )
 
         if max_steps is None:
@@ -41,6 +53,10 @@ class ContrastiveDataset(MiniGridEnv):
             **kwargs,
         )
 
+    def set_split(self, split):
+        self.curr_split = split
+        self.curr_comp_idx = 0
+
     @staticmethod
     def _gen_mission(color: str, obj_type: str):
         return f"A {color} {obj_type}"
@@ -52,33 +68,18 @@ class ContrastiveDataset(MiniGridEnv):
         self.grid.wall_rect(0, 0, width, height)
 
         # Types and colors of objects we can generate
-        types = self.obj_types
+        obj_color, obj_type = self.splits[self.curr_split][self.curr_comp_idx]
+        self.curr_comp_idx = (self.curr_comp_idx + 1) % len(self.splits[self.curr_split])
 
-        objs = []
-        obj_pos = []
-
-        # Until we have generated all the objects
-        while len(objs) < self.numObjs:
-            obj_type = self._rand_elem(types)
-            obj_color = self._rand_elem(COLOR_NAMES)
-
-            # If this object already exists, try again
-            if (obj_type, obj_color) in objs:
-                continue
-
-            obj = WorldObj.decode(OBJECT_TO_IDX[obj_type], COLOR_TO_IDX[obj_color], 0)
-
-            pos = self.place_obj(obj)
-            objs.append((obj_type, obj_color))
-            obj_pos.append(pos)
+        obj = WorldObj.decode(OBJECT_TO_IDX[obj_type], COLOR_TO_IDX[obj_color], 0)
+        pos = self.place_obj(obj)
 
         # Randomize the agent start position and orientation
         self.place_agent()
 
         # Choose a random object to be picked up
-        objIdx = self._rand_int(0, len(objs))
-        self.targetType, self.target_color = objs[objIdx]
-        self.target_pos = obj_pos[objIdx]
+        self.targetType, self.target_color = obj_type, obj_color
+        self.target_pos = pos
 
         self.label = f"A {self.target_color} {self.targetType}"
         self.mission = self.label
@@ -106,9 +107,10 @@ class ContrastiveDataset(MiniGridEnv):
 
 if __name__ == "__main__":
     import argparse
-    import gym
+    import gymnasium as gym
     from minigrid.utils.window import Window
     from PIL import Image
+    from pathlib import Path
 
     gym.register(
         id="ContrastiveDataset-v0",
@@ -129,7 +131,7 @@ if __name__ == "__main__":
         "--tile-size", type=int, help="size at which to render tiles", default=32
     )
     parser.add_argument(
-        "--num-instances", type=int, help="size of dataset", default=10000
+        "--num-instances-per-obj", type=int, help="size of dataset", default=2
     )
     parser.add_argument(
         "--agent-view",
@@ -151,25 +153,23 @@ if __name__ == "__main__":
 
     window = Window("minigrid - " + str(env.__class__))
 
-    for i in range(args.num_instances):
-        env.reset(seed=args.seed)
-        # if hasattr(env, "mission"):
-            # print("Mission: %s" % env.mission)
-            # window.set_caption(env.mission)
+    for split in ['train', 'val', 'test']:
+        env.set_split(split)
+        num_instances = len(env.splits[split]) * args.num_instances_per_obj
+        Path(f'contrastive_dataset/{split}').mkdir(parents=True, exist_ok=True)
+        print(f'creating {num_instances} for split: {split}')
+        for i in range(num_instances):
+            env.reset(seed=args.seed)
+            frame = env.get_frame(agent_pov=args.agent_view, highlight=False)
+            img = Image.fromarray(frame)
+            img.save(f'contrastive_dataset/{split}/{env.mission}.{i}.png')
+            #
+            # print(type(frame))
+            # window.show_img(frame)
+            #
+            # window.show(block=False)
 
-        frame = env.get_frame(agent_pov=args.agent_view, highlight=False)
-
-
-
-        img = Image.fromarray(frame)
-        img.save(f'contrastive_dataset/{env.mission}.({i}).png')
-        #
-        # print(type(frame))
-        # window.show_img(frame)
-        #
-        # window.show(block=False)
-
-        # input('hit enter')
+            # input('hit enter')
 
 
 
